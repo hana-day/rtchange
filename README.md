e01d7c840269e58bf37dc69ed3b798a9ee600c4a
Yusuke Hanada <hyusuk9872@gmail.com>
2017-01-14 19:25:06 +0900

    Update README and setup.py

--- README.md
+++ README.md
@@ -8,6 +8,30 @@ An implementation of real-time change-point detection in time series.
 ## Installtion
 
 ```
-$ pip install -U pip
 $ pip install -U rtchange
+```
+
+## Example
+
+```python
+>>> from rtchange import Finder
+>>> f = Finder()
+>>> # `score` method returns the iterator of score of the samples.
+>>> scores = f.score([0, 0, 0, 0])
+<generator object Finder.score at 0x7faa143c4620>
+>>> print(list(scores))
+[0.0, 0.3938543257165724, 0.42281301105991476, 0.4718002708508469]
+>>> print(list(f.score([1, 1])))
+[1.0910027829221285, 1.2528868802573792]
+>>> # You can also use `score_one` method for the one sample. 
+>>> f.score_one(2)
+1.4303444070633553
+```
+
+## Testing
+
+```
+$ pip install -U pip
+$ pip install -e .[testing]
+$ make
 ```
--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
 import os
+import sys
 
 from setuptools import setup, find_packages
+
+py_version = sys.version_info[:2]
+
+PY2 = py_version[0] == 2
+
+if PY2:
+    raise RuntimeError('On Python 2, rtchange requires Python 3.x')
 
 here = os.path.abspath(os.path.dirname(__file__))
 try:
@@ -24,7 +32,16 @@ setup(
     description='Real-time change point detection',
     long_description=README,
     classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "License :: OSI Approved :: MIT License",
     ],
+    keywords='change detection realtime',
     author="Yusuke Hanada",
     author_email="hyusuk9872@gmail.com",
     license="The MIT License (https://opensource.org/licenses/MIT)",
